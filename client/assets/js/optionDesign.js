dfe3bdabbc18a17fcfa9747291da3ee2302977ad
VoGiaHuy <huyvog6226@gmail.com>
2025-11-29 02:28:59 +0700

    update plus and minus number of product

--- client/assets/js/optionDesign.js
+++ client/assets/js/optionDesign.js
@@ -1,32 +1,50 @@
-const buttonOrder = document.querySelectorAll(".buttonOrder");
-const buttonPrint = document.querySelectorAll(".buttonPrint");
-const buttonSize = document.querySelectorAll(".buttonSize");
-const buttonColor = document.querySelectorAll(".buttonColor");
+function setupToggle(buttons) {
+  buttons.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      if (btn.classList.contains("active")) {
+        btn.classList.remove("active");
+      } else {
+        buttons.forEach((b) => b.classList.remove("active"));
+        btn.classList.add("active");
+      }
+    });
+  });
+}
 
-buttonOrder.forEach((btn) => {
+setupToggle(document.querySelectorAll(".buttonOrder"));
+setupToggle(document.querySelectorAll(".buttonPrint"));
+setupToggle(document.querySelectorAll(".buttonSize"));
+
+// color selected
+const buttonColors = document.querySelectorAll(".buttonColor");
+const colorSelected = document.querySelector(".colorSelected");
+buttonColors.forEach((btn) => {
   btn.addEventListener("click", () => {
-    buttonOrder.forEach((b) => b.classList.remove("active"));
-    btn.classList.add("active");
+    if (btn.classList.contains("active")) {
+      btn.classList.remove("active");
+      colorSelected.style.backgroundColor = "";
+    } else {
+      buttonColors.forEach((b) => b.classList.remove("active"));
+      btn.classList.add("active");
+      colorSelected.style.backgroundColor = btn.style.backgroundColor;
+    }
   });
 });
 
-buttonPrint.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    buttonPrint.forEach((b) => b.classList.remove("active"));
-    btn.classList.add("active");
-  });
+const minusBtn = document.querySelector(".chooseQuantity .minus");
+const plusBtn = document.querySelector(".chooseQuantity .plus");
+const numberDiv = document.querySelector(".chooseQuantity .number");
+
+let quantity = parseInt(numberDiv.textContent); // khởi tạo số ban đầu
+
+minusBtn.addEventListener("click", () => {
+  if (quantity > 1) {
+    quantity--;
+    numberDiv.textContent = quantity;
+  }
 });
 
-buttonSize.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    buttonSize.forEach((b) => b.classList.remove("active"));
-    btn.classList.add("active");
-  });
-});
-
-buttonColor.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    buttonColor.forEach((b) => b.classList.remove("active"));
-    btn.classList.add("active");
-  });
+plusBtn.addEventListener("click", () => {
+  quantity++;
+  numberDiv.textContent = quantity;
 });

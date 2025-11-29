08cc5f06b817cd84141f0d44b0953ac68c2e2832
VoGiaHuy <huyvog6226@gmail.com>
2025-11-30 02:13:10 +0700

    change color toolbar left

--- client/assets/js/designer.js
+++ client/assets/js/designer.js
@@ -321,3 +321,40 @@ function setupRealTimeTextSync() {
     });
   });
 }
+
+// hiệu ứng nền particles.js
+document.addEventListener("DOMContentLoaded", function () {
+  particlesJS("particles-js", {
+    particles: {
+      number: { value: 100 },
+      color: { value: "#fff" },
+      shape: { type: "circle" },
+      opacity: {
+        value: 1,
+        random: true,
+      },
+      size: {
+        value: 2,
+        random: true,
+      },
+      line_linked: {
+        opacity: 0,
+      },
+      move: {
+        enable: true,
+        speed: 9,
+      },
+    },
+    interactivity: {
+      events: {
+        onhover: { enable: true, mode: "repulse" },
+        onclick: { enable: true, mode: "push" },
+      },
+      modes: {
+        repulse: { distance: 100, duration: 0.4 },
+        push: { particles_nb: 4 },
+      },
+    },
+    retina_detect: true,
+  });
+});
